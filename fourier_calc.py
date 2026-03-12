1339aa889c46acfc1663dfdf52299ef9f1fb897e
pcarlip <carlipp@oregonstate.edu>
2026-03-12 16:27:08 -0700

    full refactor of fourier-prep to support cupy, reduce memory use

--- fourier_calc.py
+++ fourier_calc.py
@@ -36,8 +36,79 @@ class GradMethod(Enum):
     numpy = 3
 
 
+def spectral_der_gpu(vel_hat: cp.ndarray, k_grid: cp.ndarray) -> cp.ndarray:
+    return cp.real(cufft.ifftn(cufft.ifftshift(1j * k_grid * vel_hat)))
+
+
+def spectral_der_cpu(vel_hat: np.ndarray, k_grid: np.ndarray) -> np.ndarray:
+    return np.real(fft.ifftn(fft.ifftshift(1j * k_grid * vel_hat)))
+
+
+def pi_int_dir_spectral_gpu(
+    vel: cp.ndarray, dx: float, data: SimData | SimDataLite, k_range: cp.ndarray
+) -> cp.ndarray:
+    k_mesh = cp.meshgrid(k_range, k_range, k_range)
+    vel_hat = cufft.fftshift(cufft.fftn(vel))
+    adv_realspace = spectral_der_gpu(vel_hat, k_mesh[2]) * data.u  # u d(vel)/dx
+    adv_realspace += spectral_der_gpu(vel_hat, k_mesh[0]) * data.v  # +v d(vel)/dy
+    adv_realspace += spectral_der_gpu(vel_hat, k_mesh[1]) * data.w  # +w d(vel)/dz
+    adv_spec = cufft.fftshift(cufft.fftn(adv_realspace) * dx**3 / (2 * np.pi))
+    vel_conj = cp.conj(vel_hat) * dx**3 / (2 * np.pi)
+    return cp.real(vel_conj * adv_spec)
+
+
+def pi_int_dir_spectral_cpu(
+    vel: np.ndarray, dx: float, data: SimData | SimDataLite, k_range: np.ndarray
+) -> np.ndarray:
+    k_mesh = np.meshgrid(k_range, k_range, k_range)
+    vel_hat = fft.fftshift(fft.fftn(vel))
+    adv_realspace = spectral_der_cpu(vel_hat, k_mesh[2]) * data.u  # u d(vel)/dx
+    adv_realspace += spectral_der_cpu(vel_hat, k_mesh[0]) * data.v  # +v d(vel)/dy
+    adv_realspace += spectral_der_cpu(vel_hat, k_mesh[1]) * data.w  # +w d(vel)/dz
+    adv_spec = fft.fftshift(fft.fftn(adv_realspace) * dx**3 / (2 * np.pi))
+    vel_conj = np.conj(vel_hat) * dx**3 / (2 * np.pi)
+    return np.real(vel_conj * adv_spec)
+
+
+def pi_int_dir_np_gpu(
+    vel: cp.ndarray, dx: float, data: SimData | SimDataLite
+) -> cp.ndarray:
+    adv_realspace = cp.gradient(vel, dx, axis=2) * data.u
+    adv_realspace += cp.gradient(vel, dx, axis=1) * data.v
+    adv_realspace += cp.gradient(vel, dx, axis=0) * data.w
+    adv_spec = cufft.fftshift(cufft.fftn(adv_realspace) * dx**3 / (2 * np.pi))
+    vel_conj = cufft.fftshift(cp.conj(cufft.fftn(vel)) * dx**3 / (2 * np.pi))
+    return cp.real(vel_conj * adv_spec)
+
+
+def pi_int_dir_np_cpu(
+    vel: np.ndarray, dx: float, data: SimData | SimDataLite
+) -> np.ndarray:
+    adv_realspace = np.gradient(vel, dx, axis=2) * data.u
+    adv_realspace += np.gradient(vel, dx, axis=1) * data.v
+    adv_realspace += np.gradient(vel, dx, axis=0) * data.w
+    adv_spec = fft.fftshift(fft.fftn(adv_realspace) * dx**3 / (2 * np.pi))
+    vel_conj = fft.fftshift(np.conj(fft.fftn(vel)) * dx**3 / (2 * np.pi))
+    return np.real(vel_conj * adv_spec)
+
+
+def pi_int_dir_ocean_gpu(vel: cp.ndarray, dx: float, adv: cp.ndarray) -> cp.ndarray:
+    adv_realspace = adv
+    adv_spec = cufft.fftshift(cufft.fftn(adv_realspace) * dx**3 / (2 * np.pi))
+    vel_conj = cufft.fftshift(cp.conj(cufft.fftn(vel)) * dx**3 / (2 * np.pi))
+    return cp.real(vel_conj * adv_spec)
+
+
+def pi_int_dir_ocean_cpu(vel: np.ndarray, dx: float, adv: np.ndarray) -> np.ndarray:
+    adv_realspace = adv
+    adv_spec = fft.fftshift(fft.fftn(adv_realspace) * dx**3 / (2 * np.pi))
+    vel_conj = fft.fftshift(np.conj(fft.fftn(vel)) * dx**3 / (2 * np.pi))
+    return np.real(vel_conj * adv_spec)
+
+
 def fourier_prep(
-    data: SimData | SimDataLite, grad_method: GradMethod = GradMethod.oceananigans
+    data: SimData | SimDataLite,
+    grad_method: GradMethod = GradMethod.oceananigans,
 ) -> tuple[np.ndarray, np.ndarray]:
     """Calculate integrand for spectral flux via fourier methods
 
@@ -54,9 +125,7 @@ def fourier_prep(
         Array of (k^2 + l^2 + m^2) for each integrand value
     """
     xp = cp.get_array_module(data.u)
-    if xp.__name__ == "cupy":
-        print("hello")
-        fft.set_global_backend(cufft)  # type: ignore
+    use_gpu = xp.__name__ == "cupy"
 
     N = len(data.u)
     dx = float(data.x[1] - data.x[0])
@@ -64,60 +133,47 @@ def fourier_prep(
     dk = 2 * np.pi / L
     k_range = xp.array(fft.fftshift(fft.fftfreq(N) * N * dk))
 
-    u_conj = fft.fftshift(xp.conj(fft.fftn(data.u)) * dx**3 / (2 * np.pi))
-    v_conj = fft.fftshift(xp.conj(fft.fftn(data.v)) * dx**3 / (2 * np.pi))
-    w_conj = fft.fftshift(xp.conj(fft.fftn(data.w)) * dx**3 / (2 * np.pi))
     # dx^3 converts DFT to analog to FT, fftshift moves k = 0 to the middle
     # not sure about the factors of 2π, those come from
     # https://github.com/BrodiePearson/Paper_Bessel_SF_Method/blob/main/analysis/Calculate_Spectral_Fluxes_2D.m
 
     if grad_method == GradMethod.spectral:
-        # see above link for calculations of gradients from FFT of u
-        u_hat = fft.fftshift(fft.fftn(data.u))
-        v_hat = fft.fftshift(fft.fftn(data.v))
-        w_hat = fft.fftshift(fft.fftn(data.w))
-        k_mesh = xp.meshgrid(k_range, k_range, k_range)
-        dudx = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[2] * u_hat)))
-        dudy = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[0] * u_hat)))
-        dudz = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[1] * u_hat)))
-        dvdx = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[2] * v_hat)))
-        dvdy = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[0] * v_hat)))
-        dvdz = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[1] * v_hat)))
-        dwdx = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[2] * w_hat)))
-        dwdy = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[0] * w_hat)))
-        dwdz = xp.real(fft.ifftn(fft.ifftshift(1j * k_mesh[1] * w_hat)))
-        u_adv_realspace = data.u * dudx + data.v * dudy + data.w * dudz
-        v_adv_realspace = data.u * dvdx + data.v * dvdy + data.w * dvdz
-        w_adv_realspace = data.u * dwdx + data.v * dwdy + data.w * dwdz
-        u_adv = fft.fftshift(fft.fftn(u_adv_realspace) * dx**3 / (2 * np.pi))
-        v_adv = fft.fftshift(fft.fftn(v_adv_realspace) * dx**3 / (2 * np.pi))
-        w_adv = fft.fftshift(fft.fftn(w_adv_realspace) * dx**3 / (2 * np.pi))
+        if use_gpu:
+            pi_int = pi_int_dir_spectral_gpu(data.u, dx, data, k_range)
+            pi_int += pi_int_dir_spectral_gpu(data.v, dx, data, k_range)
+            pi_int += pi_int_dir_spectral_gpu(data.w, dx, data, k_range)
+        else:
+            pi_int = pi_int_dir_spectral_cpu(data.u, dx, data, k_range)
+            pi_int += pi_int_dir_spectral_cpu(data.v, dx, data, k_range)
+            pi_int += pi_int_dir_spectral_cpu(data.w, dx, data, k_range)
     elif grad_method == GradMethod.numpy:
-        dudz, dudy, dudx = xp.gradient(data.u, dx)
-        dvdz, dvdy, dvdx = xp.gradient(data.v, dx)
-        dwdz, dwdy, dwdx = xp.gradient(data.w, dx)
-        u_adv_realspace = data.u * dudx + data.v * dudy + data.w * dudz
-        v_adv_realspace = data.u * dvdx + data.v * dvdy + data.w * dvdz
-        w_adv_realspace = data.u * dwdx + data.v * dwdy + data.w * dwdz
-        u_adv = fft.fftshift(fft.fftn(u_adv_realspace) * dx**3 / (2 * np.pi))
-        v_adv = fft.fftshift(fft.fftn(v_adv_realspace) * dx**3 / (2 * np.pi))
-        w_adv = fft.fftshift(fft.fftn(w_adv_realspace) * dx**3 / (2 * np.pi))
+        if use_gpu:
+            pi_int = pi_int_dir_np_gpu(data.u, dx, data)
+            pi_int += pi_int_dir_np_gpu(data.v, dx, data)
+            pi_int += pi_int_dir_np_gpu(data.w, dx, data)
+        else:
+            pi_int = pi_int_dir_np_cpu(data.u, dx, data)
+            pi_int += pi_int_dir_np_cpu(data.v, dx, data)
+            pi_int += pi_int_dir_np_cpu(data.w, dx, data)
     elif grad_method == GradMethod.oceananigans:
         if type(data) is SimData:
-            u_adv = fft.fftshift(fft.fftn(data.uadv) * dx**3 / (2 * np.pi))
-            v_adv = fft.fftshift(fft.fftn(data.vadv) * dx**3 / (2 * np.pi))
-            w_adv = fft.fftshift(fft.fftn(data.wadv) * dx**3 / (2 * np.pi))
+            if use_gpu:
+                pi_int = pi_int_dir_ocean_gpu(data.u, dx, data.uadv)
+                pi_int += pi_int_dir_ocean_gpu(data.v, dx, data.vadv)
+                pi_int += pi_int_dir_ocean_gpu(data.w, dx, data.wadv)
+            else:
+                pi_int = pi_int_dir_ocean_cpu(data.u, dx, data.uadv)
+                pi_int += pi_int_dir_ocean_cpu(data.v, dx, data.vadv)
+                pi_int += pi_int_dir_ocean_cpu(data.w, dx, data.wadv)
         else:
             raise Exception("Include advection arrays to use Oceananigans gradients")
     else:
         raise Exception("Something is wrong with your gradient method choice")
 
-    pi_int = xp.real(u_conj * u_adv + v_conj * v_adv + w_conj * w_adv)
     # you can get Π by integrating Re[FT(u)* • FT((u•∇)u)]
 
-    k_grid = np.zeros((N, N, N))
-    for i, j, k in np.ndindex(N, N, N):
-        k_grid[i, j, k] = k_range[i] ** 2 + k_range[j] ** 2 + k_range[k] ** 2
+    k_mesh = xp.meshgrid(k_range, k_range, k_range)
+    k_grid = k_mesh[0] ** 2 + k_mesh[1] ** 2 + k_mesh[2] ** 2
 
     return (pi_int, k_grid)
 
