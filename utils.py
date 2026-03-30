bf76738b72933c3b425a5b845cd011565290dc74
pcarlip <carlipp@oregonstate.edu>
2026-03-29 19:40:50 -0700

    add meshgrid selction function

--- utils.py
+++ utils.py
@@ -58,3 +58,29 @@ class SFType(StrEnum):
 class IntMethod(Enum):
     simpson = 0
     addition = 1
+
+
+def meshgrid_sel(grid: tuple, axis: Axis) -> ndarray:
+    """Get the appropriate element from a 3d meshgrid to use in a spectral gradient
+    (I don't know why it works like that, it just does)
+
+    Parameters
+    ----------
+    grid : tuple[ndarray, ndarray, ndarray]
+        meshgrid constructed as (y,z,x)
+        (necessary to give the right shape when indexed as (z,y,x))
+    axis : Axis
+        Axis along which you're taking the spectral gradient
+
+    Returns
+    -------
+    ndarray
+        one element from the meshgrid,
+        appropriate for spectral derivatives along the given axis
+    """
+    if axis == Axis.x:
+        return grid[2]
+    elif axis == Axis.y:
+        return grid[0]
+    else:
+        return grid[1]
