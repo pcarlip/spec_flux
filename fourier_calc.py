676e956fc1a9278283f1c73d1b18079511fa4216
pcarlip <carlipp@oregonstate.edu>
2026-04-01 13:56:02 -0700

    unify integrand calc using general advection calc

--- fourier_calc.py
+++ fourier_calc.py
@@ -1,17 +1,18 @@
 from collections.abc import Callable
 
-from .advection import spectral_der
+from .advection import advection, spectral_der
 from .utils import Axis, GradMethod, SimData, SimDataLite, ndarray, xp_fft
 
 
-def pi_int_dir_spectral(
+def pi_int_dir(
+    method: GradMethod,
     data: SimData | SimDataLite,
     axis: Axis,
     spacings: tuple[float, ...],
     k_ranges: tuple[ndarray, ndarray, ndarray],
 ) -> ndarray:
     """Generate a component of  array to integrate: Re[FT(u)* • FT((u•∇)u)],
-    with optional cupy acceleration, using spectral estimate of gradients
+    with optional cupy acceleration
 
     Parameters
     ----------
@@ -31,117 +32,12 @@ def pi_int_dir_spectral(
     """
     vel = (data.w, data.v, data.u)[axis.value]
     xp, genfft = xp_fft(vel)
-    k_mesh = xp.meshgrid(*k_ranges, indexing="ij")
     vel_hat = genfft.fftshift(genfft.fftn(vel))
-    adv_realspace = spectral_der(vel_hat, k_mesh[2]) * data.u  # u d(vel)/dx
-    adv_realspace += spectral_der(vel_hat, k_mesh[1]) * data.v  # +v d(vel)/dy
-    adv_realspace += spectral_der(vel_hat, k_mesh[0]) * data.w  # +w d(vel)/dz
+    adv_realspace = advection(data, axis, spacings, k_ranges, method)
     dxdydz = spacings[0] * spacings[1] * spacings[2]
     adv_spec = genfft.fftshift(genfft.fftn(adv_realspace) * dxdydz / (2 * xp.pi))
     vel_conj = xp.conj(vel_hat) * dxdydz / (2 * xp.pi)
     return xp.real(vel_conj * adv_spec)
-
-
-def pi_int_dir_finite(
-    data: SimData | SimDataLite,
-    axis: Axis,
-    spacings: tuple[float, ...],
-    k_ranges: tuple[ndarray, ndarray, ndarray],
-) -> ndarray:
-    """Generate a component of  array to integrate: Re[FT(u)* • FT((u•∇)u)],
-    with optional cupy acceleration, using numpy finite element estimate of gradients
-
-    Parameters
-    ----------
-    data : SimData | SimDataLite
-        Object containing realspace velocity components
-    axis : Axis
-        Axis along which to generate the component
-    spacings : tuple[float, float, float]
-        Grid spacings
-    k_ranges : tuple[ndarray, ndarray, ndarray]
-        Unused, for API compatability
-
-    Returns
-    -------
-    ndarray
-        Directional component of Re[FT(u)* • FT((u•∇)u)]
-    """
-    vel = (data.w, data.v, data.u)[axis.value]
-    xp, genfft = xp_fft(vel)
-    adv_realspace = xp.gradient(vel, spacings[2], axis=2) * data.u
-    adv_realspace += xp.gradient(vel, spacings[1], axis=1) * data.v
-    adv_realspace += xp.gradient(vel, spacings[0], axis=0) * data.w
-    dxdydz = spacings[0] * spacings[1] * spacings[2]
-    adv_spec = genfft.fftshift(genfft.fftn(adv_realspace) * dxdydz / (2 * xp.pi))
-    vel_conj = genfft.fftshift(xp.conj(genfft.fftn(vel)) * dxdydz / (2 * xp.pi))
-    return xp.real(vel_conj * adv_spec)
-
-
-def pi_int_dir_ocean(
-    data: SimData,
-    axis: Axis,
-    spacings: tuple[float, ...],
-    k_ranges: tuple[ndarray, ndarray, ndarray],
-) -> ndarray:
-    """Generate a component of  array to integrate: Re[FT(u)* • FT((u•∇)u)],
-    using provided gradients (e.g. calculated during the simulation), with optional
-    cupy acceleration
-
-    Parameters
-    ----------
-    data : SimData | SimDataLite
-        Object containing realspace velocity and advection components
-    axis : Axis
-        Axis along which to generate the component
-    spacings : tuple[float, float, float]
-        Grid spacings
-    k_ranges : tuple[ndarray, ndarray, ndarray]
-        Unused, for API compatability
-
-    Returns
-    -------
-    ndarray
-        Directional component of Re[FT(u)* • FT((u•∇)u)]
-    """
-    vel = (data.w, data.v, data.u)[axis.value]
-    adv = (data.wadv, data.vadv, data.uadv)[axis.value]
-    xp, genfft = xp_fft(vel)
-    dxdydz = spacings[0] * spacings[1] * spacings[2]
-    adv_spec = genfft.fftshift(genfft.fftn(adv) * dxdydz / (2 * xp.pi))
-    vel_conj = genfft.fftshift(xp.conj(genfft.fftn(vel)) * dxdydz / (2 * xp.pi))
-    return xp.real(vel_conj * adv_spec)
-
-
-def pi_int_func(
-    data: SimData | SimDataLite,
-    spacings: tuple[float, ...],
-    k_ranges: tuple[ndarray, ndarray, ndarray],
-    directional: Callable,
-) -> ndarray:
-    """Generate array of integrand values for fourier-derived spectral flux
-
-    Parameters
-    ----------
-    data : SimData | SimDataLite
-        Dataclass with velocity and (optionally) advection data
-    spacings : tuple[float, ...]
-        Grid spacings
-    ranges : tuple[ndarray, ndarray, ndarray]
-        Range of m-values (z), l-values (y), k-values (x) associated with the grid size
-    directional : Callable
-        Function to calculate directional component of integrand
-        Must have signature (SimData | SimDataLite, Axis, spacings, ranges)
-
-    Returns
-    -------
-    ndarray
-        Array of integrand values
-    """
-    pi_int = directional(data, Axis.x, spacings, k_ranges)
-    pi_int += directional(data, Axis.y, spacings, k_ranges)
-    pi_int += directional(data, Axis.z, spacings, k_ranges)
-    return pi_int
 
 
 def fourier_prep(
@@ -179,20 +75,7 @@ def fourier_prep(
     # not sure about the factors of 2π, those come from
     # https://github.com/BrodiePearson/Paper_Bessel_SF_Method/blob/main/analysis/Calculate_Spectral_Fluxes_2D.m
 
-    if grad_method == GradMethod.spectral:
-        pi_int = pi_int_func(data, spacings, ranges, pi_int_dir_spectral)
-    elif grad_method == GradMethod.numpy:
-        pi_int = pi_int_func(data, spacings, ranges, pi_int_dir_finite)
-    elif grad_method == GradMethod.oceananigans:
-        if type(data) is SimData:
-            pi_int = pi_int_func(data, spacings, ranges, pi_int_dir_ocean)
-        else:
-            raise Exception(
-                "Include advection arrays to use Oceananigans or other precalculated gradients"
-            )
-    else:
-        raise Exception("Something is wrong with your gradient method choice")
-
+    pi_int = sum(pi_int_dir(grad_method, data, axis, spacings, ranges) for axis in Axis)
     # you can get Π by integrating Re[FT(u)* • FT((u•∇)u)]
 
     k_mesh = xp.meshgrid(*ranges, indexing="ij")
