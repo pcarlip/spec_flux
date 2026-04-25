e9fe9dc2a243eda007f34dfd3b65d7624cb22002
pcarlip <carlipp@oregonstate.edu>
2026-04-25 04:05:41 -0700

    improve directional test, add xarray tests

--- test_sf_au.py
+++ test_sf_au.py
@@ -1,11 +1,13 @@
 import sys
 
 import cupy as cp
+import cupy_xarray
 import fluidsf
 import numpy as np
+import xarray as xr
 
 sys.path.append("..")
-from numerics.sf_au import Axis, sf_au, sf_au_dir
+from numerics.sf_au import Axis, sf_au, sf_au_dir, sf_au_dir_xr
 
 
 def test_minimal() -> None:
@@ -83,6 +85,64 @@ def test_fluidsf_comp_dir() -> None:
     )
 
 
+def test_fluidsf_xr_dir() -> None:
+    """Structure function results along only one axis should match fluidsf"""
+    rng = np.random.default_rng(31415)
+    u = rng.normal(size=(10, 11, 12))
+    v = rng.normal(size=(10, 11, 12))
+    w = rng.normal(size=(10, 11, 12))
+    x = np.arange(12)
+    y = np.arange(11)
+    z = np.arange(10)
+
+    dims = ("z_aac", "y_aca", "x_caa")
+
+    ds = xr.Dataset(
+        {"u": (dims, u), "v": (dims, v), "w": (dims, w)},
+        coords={"z_aac": z, "y_aca": y, "x_caa": x},
+    )
+
+    fsf = fluidsf.generate_structure_functions_3d(u, v, w, x, y, z, ["ASF_V"])
+    np.testing.assert_allclose(
+        sf_au_dir_xr(ds, Axis.x).data, fsf["SF_advection_velocity_x"]
+    )
+    np.testing.assert_allclose(
+        sf_au_dir_xr(ds, Axis.y).data, fsf["SF_advection_velocity_y"]
+    )
+    np.testing.assert_allclose(
+        sf_au_dir_xr(ds, Axis.z).data, fsf["SF_advection_velocity_z"]
+    )
+
+
+def test_fluidsf_xr_dir_cp() -> None:
+    """Structure function results along only one axis should match fluidsf"""
+    rng = np.random.default_rng(31415)
+    u = rng.normal(size=(10, 11, 12))
+    v = rng.normal(size=(10, 11, 12))
+    w = rng.normal(size=(10, 11, 12))
+    x = np.arange(12)
+    y = np.arange(11)
+    z = np.arange(10)
+
+    dims = ("z_aac", "y_aca", "x_caa")
+
+    ds = xr.Dataset(
+        {"u": (dims, u), "v": (dims, v), "w": (dims, w)},
+        coords={"z_aac": z, "y_aca": y, "x_caa": x},
+    ).as_cupy()
+
+    fsf = fluidsf.generate_structure_functions_3d(u, v, w, x, y, z, ["ASF_V"])
+    np.testing.assert_allclose(
+        sf_au_dir_xr(ds, Axis.x).data.get(), fsf["SF_advection_velocity_x"]
+    )
+    np.testing.assert_allclose(
+        sf_au_dir_xr(ds, Axis.y).data.get(), fsf["SF_advection_velocity_y"]
+    )
+    np.testing.assert_allclose(
+        sf_au_dir_xr(ds, Axis.z).data.get(), fsf["SF_advection_velocity_z"]
+    )
+
+
 def test_cupy_comp() -> None:
     """Cupy and numpy calculations should agree"""
     rng = np.random.default_rng(31415)
@@ -123,7 +183,17 @@ def test_cupy_comp_dir() -> None:
     cy = cp.array(y)
     cz = cp.array(z)
 
-    np_sf = sf_au_dir(u, v, w, x, y, z, Axis.x)
-    cp_sf = sf_au_dir(cu, cv, cw, cx, cy, cz, Axis.x)
+    np_sf_x = sf_au_dir(u, v, w, x, y, z, Axis.x)
+    cp_sf_x = sf_au_dir(cu, cv, cw, cx, cy, cz, Axis.x)
 
-    np.testing.assert_allclose(np_sf[1], cp.asnumpy(cp_sf[1]))
+    np.testing.assert_allclose(np_sf_x[1], cp.asnumpy(cp_sf_x[1]))
+
+    np_sf_y = sf_au_dir(u, v, w, x, y, z, Axis.y)
+    cp_sf_y = sf_au_dir(cu, cv, cw, cx, cy, cz, Axis.y)
+
+    np.testing.assert_allclose(np_sf_y[1], cp.asnumpy(cp_sf_y[1]))
+
+    np_sf_z = sf_au_dir(u, v, w, x, y, z, Axis.z)
+    cp_sf_z = sf_au_dir(cu, cv, cw, cx, cy, cz, Axis.z)
+
+    np.testing.assert_allclose(np_sf_z[1], cp.asnumpy(cp_sf_z[1]))
