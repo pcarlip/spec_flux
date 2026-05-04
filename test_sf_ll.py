4562aa983201196461a53067d881c7ab4a64c630
pcarlip <carlipp@oregonstate.edu>
2026-05-04 01:02:04 -0700

    add, test directional xarray sf

--- test_sf_ll.py
+++ test_sf_ll.py
@@ -1,12 +1,14 @@
 import sys
 
 import cupy as cp
+import cupy_xarray
 import fluidsf
 import numpy as np
+import xarray as xr
 
 sys.path.append("..")
 from numerics.sf_au import Axis
-from numerics.sf_ln import sf_ln, sf_ln_dir
+from numerics.sf_ln import sf_ln, sf_ln_dir, sf_ln_dir_xr
 
 
 def test_minimal() -> None:
@@ -140,3 +142,49 @@ def test_cupy_comp_dir() -> None:
     for i in range(3):
         for j in range(2):
             assert np.isclose(np_sf[i][j], cp.asnumpy(cp_sf[i][j])).all()
+
+
+def test_fluidsf_xr_dir() -> None:
+    """Structure function results along only one axis should match fluidsf"""
+    rng = np.random.default_rng(31415)
+    u = rng.normal(size=(10, 11, 12))
+    v = rng.normal(size=(10, 11, 12))
+    w = rng.normal(size=(10, 11, 12))
+    x = np.arange(12)
+    y = np.arange(11)
+    z = np.arange(10)
+
+    dims = ("z_aac", "y_aca", "x_caa")
+
+    ds = xr.Dataset(
+        {"u": (dims, u), "v": (dims, v), "w": (dims, w)},
+        coords={"z_aac": z, "y_aca": y, "x_caa": x},
+    )
+
+    fsf = fluidsf.generate_structure_functions_3d(u, v, w, x, y, z, ["LL"])
+    np.testing.assert_allclose(sf_ln_dir_xr(ds, Axis.x, 2).data, fsf["SF_LL_x"])
+    np.testing.assert_allclose(sf_ln_dir_xr(ds, Axis.y, 2).data, fsf["SF_LL_y"])
+    np.testing.assert_allclose(sf_ln_dir_xr(ds, Axis.z, 2).data, fsf["SF_LL_z"])
+
+
+def test_fluidsf_xr_dir_cp() -> None:
+    """Structure function results along only one axis should match fluidsf"""
+    rng = np.random.default_rng(31415)
+    u = rng.normal(size=(10, 11, 12))
+    v = rng.normal(size=(10, 11, 12))
+    w = rng.normal(size=(10, 11, 12))
+    x = np.arange(12)
+    y = np.arange(11)
+    z = np.arange(10)
+
+    dims = ("z_aac", "y_aca", "x_caa")
+
+    ds = xr.Dataset(
+        {"u": (dims, u), "v": (dims, v), "w": (dims, w)},
+        coords={"z_aac": z, "y_aca": y, "x_caa": x},
+    ).as_cupy()
+
+    fsf = fluidsf.generate_structure_functions_3d(u, v, w, x, y, z, ["LL"])
+    np.testing.assert_allclose(sf_ln_dir_xr(ds, Axis.x, 2).data.get(), fsf["SF_LL_x"])
+    np.testing.assert_allclose(sf_ln_dir_xr(ds, Axis.y, 2).data.get(), fsf["SF_LL_y"])
+    np.testing.assert_allclose(sf_ln_dir_xr(ds, Axis.z, 2).data.get(), fsf["SF_LL_z"])
