298c66b9f2b0ca50903b04f9f714c355dc2d633a
pcarlip <carlipp@oregonstate.edu>
2026-06-11 02:56:43 -0700

    add docstrings, use xarray outputs

--- fourier_calc.py
+++ fourier_calc.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from typing import Literal
 
 import cupy_xarray
@@ -63,6 +64,25 @@ def pi_int_dir_xr(
     method: GradMethod,
     edge_order: Literal[1, 2] = 2,
 ) -> xr.DataArray:
+    """Generate a component of  array to integrate: Re[FT(u)* • FT((u•∇)u)],
+    with optional cupy acceleration
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        Dataset with realspace velocity components (on the same grid)
+    axis : Axis
+        Axis along which to generate the component
+    method : GradMethod
+        Method for calculating gradients
+    edge_order : Literal[1, 2], optional
+        Order of finite difference gradients at domain edges, by default 2
+
+    Returns
+    -------
+    xr.DataArray
+        Directional component of Re[FT(u)* • FT((u•∇)u)]
+    """
     vel = (data.w, data.v, data.u)[axis.value]
     adv_realspace = advection_xr(data, axis, method, edge_order=edge_order)
     vel_hat = np.conj(xrft.fft(vel))
@@ -85,7 +105,7 @@ def fourier_prep(
     grad_method : GradMethod, optional
         Method for calculating gradients, by default GradMethod.numpy
     edge_order : Literal[1, 2], optional
-        Order of finite difference gradients, by default 2
+        Order of finite difference gradients at domain edges, by default 2
 
     Returns
     -------
@@ -117,6 +137,23 @@ def fourier_prep_xr(
     grad_method: GradMethod = GradMethod.numpy,
     edge_order: Literal[1, 2] = 2,
 ) -> xr.Dataset:
+    """Calculate integrand for spectral flux via fourier methods, with optional cupy
+    acceleration
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        Dataset with realspace velocity components (on the same grid)
+    grad_method : GradMethod, optional
+        Method for calculating gradients, by default GradMethod.numpy
+    edge_order : Literal[1, 2], optional
+        Order of finite difference gradients at domain edges, by default 2
+
+    Returns
+    -------
+    xr.Dataset
+        Dataset with integrand values, k magnitudes
+    """
     pi_int: xr.DataArray = sum(
         pi_int_dir_xr(data, axis, grad_method, edge_order) for axis in Axis
     ).rename("pi_int")  # type: ignore
@@ -162,7 +199,42 @@ def fourier_int(
     return float(xp.sum(masked_array) * dk**3 / (2 * xp.pi * L**3))
 
 
-def fourier_int_xr(data: xr.Dataset, klim: float) -> float:
+def fourier_int_xr(data: xr.Dataset, klim: float) -> xr.DataArray:
+    """Calculate spectral flux of energy dissipation through a fourier transform
+    at a specific wavenumber
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        Dataset containing integrand, k magnitudes
+    klim : float
+        K value to integrate up to
+
+    Returns
+    -------
+    xr.DataArray
+        Spectral flux at klim
+    """
     masked = data["pi_int"].where(data["k"] <= (klim / (2 * np.pi)) ** 2, 0.0)
     val = masked.integrate(["freq_x_caa", "freq_y_aca", "freq_z_aac"])
-    return np.real(val.item()) / data.L**3
+    num = np.real(val.item()) / data.L**3
+    return xr.DataArray(num, {"time": data.time, "k": klim})
+
+
+def fourier_int_xr_lst(data: xr.Dataset, k_lst: Iterable[float]) -> xr.DataArray:
+    """Calculate spectral flux of energy dissipation through a fourier transform
+    over a list of wavenumbers
+
+    Parameters
+    ----------
+    data : xr.Dataset
+        Dataset containing integrand, k magnitudes
+    k_lst : Iterable[float]
+        List of k values to integrate to
+
+    Returns
+    -------
+    xr.DataArray
+        Spectral flux as a function of k, at given k values
+    """
+    return xr.concat([fourier_int_xr(data, k) for k in k_lst], "k")
