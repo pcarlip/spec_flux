3ec025b5f91762c66c7fe22c4bf33e20498a2821
pcarlip <carlipp@oregonstate.edu>
2026-07-22 12:56:50 -0700

    debug print should flush to log

--- sf_ln.py
+++ sf_ln.py
@@ -367,7 +367,7 @@ def sf_ln_xr(
 
     for ni, i in enumerate(zind):
         if i % 5 == 0 and debug_print:
-            print(i)
+            print(i, flush=True)
         for nj, j in enumerate(yind):
             for nk, k in enumerate(xind):
                 if not (i == 0 and j == 0 and k == 0):
