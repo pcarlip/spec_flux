e8081b0b6c358bc158d4f09b85db9a14f9178b10
pcarlip <carlipp@oregonstate.edu>
2026-09-01 01:09:27 -0700

    use xarray sfs for fluidsf compat

--- src/spec_flux/fluidsf_compat.py
+++ src/spec_flux/fluidsf_compat.py
@@ -1,7 +1,9 @@
 import cupy as cp
+import numpy as np
+import xarray as xr
 
-from .sf_au import sf_au_dir
-from .sf_ln import sf_ln_dir
+from .sf_au import sf_au_dir_xr
+from .sf_ln import sf_ln_dir_xr
 from .utils import Axis, ndarray
 
 
@@ -13,7 +15,14 @@ def fluidsf_compat(
     y: ndarray,
     z: ndarray,
     sf_names: list[str],
-) -> dict[str, ndarray]:
+) -> dict[str, np.ndarray]:
+
+    dims = ("z_aac", "y_aca", "x_caa")
+    ds = xr.Dataset(
+        {"u": (dims, u), "v": (dims, v), "w": (dims, w)},
+        coords={"z_aac": z, "y_aca": y, "x_caa": x},
+    )
+
     L = len(z) // 2
     M = len(y) // 2
     N = len(x) // 2
@@ -24,17 +33,17 @@ def fluidsf_compat(
 
     out = {"x-diffs": dx, "y-diffs": dy, "z-diffs": dz}
     if "ASF_V" in sf_names:
-        out["SF_advection_velocity_x"] = sf_au_dir(u, v, w, x, y, z, Axis.x)[1]
-        out["SF_advection_velocity_y"] = sf_au_dir(u, v, w, x, y, z, Axis.y)[1]
-        out["SF_advection_velocity_z"] = sf_au_dir(u, v, w, x, y, z, Axis.z)[1]
+        out["SF_advection_velocity_x"] = sf_au_dir_xr(ds, Axis.x).data
+        out["SF_advection_velocity_y"] = sf_au_dir_xr(ds, Axis.y).data
+        out["SF_advection_velocity_z"] = sf_au_dir_xr(ds, Axis.z).data
     if "LLL" in sf_names:
-        out["SF_LLL_x"] = sf_ln_dir(u, v, w, x, y, z, Axis.x, 3)[1]
-        out["SF_LLL_y"] = sf_ln_dir(u, v, w, x, y, z, Axis.y, 3)[1]
-        out["SF_LLL_z"] = sf_ln_dir(u, v, w, x, y, z, Axis.z, 3)[1]
+        out["SF_LLL_x"] = sf_ln_dir_xr(ds, Axis.x, 3).data
+        out["SF_LLL_y"] = sf_ln_dir_xr(ds, Axis.y, 3).data
+        out["SF_LLL_z"] = sf_ln_dir_xr(ds, Axis.z, 3).data
     if "LL" in sf_names:
-        out["SF_LL_x"] = sf_ln_dir(u, v, w, x, y, z, Axis.x, 2)[1]
-        out["SF_LL_y"] = sf_ln_dir(u, v, w, x, y, z, Axis.y, 2)[1]
-        out["SF_LL_z"] = sf_ln_dir(u, v, w, x, y, z, Axis.z, 2)[1]
+        out["SF_LL_x"] = sf_ln_dir_xr(ds, Axis.x, 2).data
+        out["SF_LL_y"] = sf_ln_dir_xr(ds, Axis.y, 2).data
+        out["SF_LL_z"] = sf_ln_dir_xr(ds, Axis.z, 2).data
 
     if cp.get_array_module(u).__name__ == "cupy":
         for key, val in out.items():
